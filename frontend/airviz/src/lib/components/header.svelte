a09613203d8944339fea9fe9febb82eca7277a99
Lily Chau <chauyeeki@gmail.com>
2025-08-11 12:55:44 +0100

    Added header

---
+++ frontend/airviz/src/lib/components/header.svelte
@@ -0,0 +1,26 @@
+<script>
+  import logoUrl from '../../assets/logo.svg?url';
+  import DropdownIcon from '../../assets/dropdown.svg?component';
+</script>
+
+<header class="site-header">
+  <div class="header-content">
+    <img src={logoUrl} alt="AirViz Logo" class="logo" />
+    <nav>
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+        <li class="dropdown">
+          <a href="/data">
+            Data <DropdownIcon class="dropdown-icon" />
+          </a>
+          <ul class="dropdown-content">
+            <li><a href="/data/overview">Overview</a></li>
+            <li><a href="/data/map">Map</a></li>
+          </ul>
+        </li>
+        <li><a href="/contact">Contact</a></li>
+      </ul>
+    </nav>
+  </div>
+</header>
--- frontend/airviz/vite.config.js
+++ frontend/airviz/vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import svg from '@poppanator/sveltekit-svg';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
-})
+  plugins: [
+    svelte(),
+    svg() // options optional
+  ]
+});
